48bbe542f2e25ee3df68c5a79b7474b055675083
Hüseyin Polat <usainpolat@gmail.com>
2026-05-10 17:38:20 +0300

    kullanılmayan alan ve fonksiyonlar silindi

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,14 +6,13 @@ import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
-    private final By searchBoxTrigger = AppiumBy.id("com.akakce.akakce:id/searchTextView");
-    private final By searchBoxInput = AppiumBy.id("com.akakce.akakce:id/searchTextView");
+    private final By searchBox = AppiumBy.id("com.akakce.akakce:id/searchTextView");
 
     public HomePage(AndroidDriver driver) { super(driver); }
 
     public ProductListPage searchForProduct(String productName) {
-        waitForClickability(searchBoxTrigger).click();
-        waitForVisibility(searchBoxInput).sendKeys(productName);
+        waitForClickability(searchBox).click();
+        waitForVisibility(searchBox).sendKeys(productName);
         driver.executeScript("mobile: performEditorAction", ImmutableMap.of("action", "search"));
         return new ProductListPage(driver);
     }
